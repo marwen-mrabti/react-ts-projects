dacf44f17a12c1403904396e112c3026b78af1f0
Marwen-Mrabti-chantier <m.mrabti@chantier.tn>
2023-06-23 11:38:00 +0100

    steps

--- src/App.tsx
+++ src/App.tsx
@@ -10,21 +10,32 @@ const messages = [
 
 function App() {
   const [active, setActive] = useState(1);
-  return (
-    <ul className="steps">
-      <div className="numbers">
-        {messages.map((_, index) => (
-          <React.Fragment key={index}>
-            <div className={active === index + 1 ? "active" : ""}>{index + 1}</div>
-          </React.Fragment>
-        ))}
-      </div>
+  const [open, setOpen] = useState(false);
 
-      <p className="message">
-        step {active}: {messages[active - 1]}
-      </p>
-      <Controls totalSteps={messages.length} active={active} setActive={setActive} />
-    </ul>
+  return (
+    <>
+      <button className="close" onClick={() => setOpen((prev) => !prev)}>
+        &times;
+      </button>
+      {!open ? null : (
+        <ul className="steps">
+          <div className="numbers">
+            {messages.map((_, index) => (
+              <React.Fragment key={index}>
+                <div className={`${active === index + 1 ? "active" : ""}`}>
+                  {index + 1}
+                </div>
+              </React.Fragment>
+            ))}
+          </div>
+
+          <p className="message">
+            step {active}: {messages[active - 1]}
+          </p>
+          <Controls totalSteps={messages.length} active={active} setActive={setActive} />
+        </ul>
+      )}
+    </>
   );
 }
 
