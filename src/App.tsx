00559dea709105fad59b772335f46318eee7bd73
Marwen-Mrabti-chantier <m.mrabti@chantier.tn>
2023-07-03 09:00:19 +0100

    travel list init

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import React from "react";
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 function App() {
-  return <div className="container"></div>;
+  return <div className=""></div>;
 }
 
 export default App;
